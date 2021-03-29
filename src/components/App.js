b42b087fd6b3c18477dfe86b13641dcfa621704b
Denis Zinoviev <Examle20@yandex.ru>
2021-03-29 22:59:07 +0300

    Инициализация проекта

---
+++ src/components/App.js
@@ -0,0 +1,248 @@
+import React from "react";
+import '../App.css';
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import ImagePopup from "./ImagePopup";
+import EditProfilePopup from "./EditProfilePopup";
+import AddPlacePopup from "./AddPlacePopup";
+import EditAvatarPopup from "./EditAvatarPopup";
+import CardDeletePopup from "./CardDeletePopup";
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
+import api from "../utils/api";
+
+function App() {
+
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+  const [isCardDeletePopupOpen, setIsCardDeletePopupOpen] = React.useState(false);
+
+  const [selectedCard, setSelectedCard] = React.useState({link:'', name: ''});
+  const [isPopupWithImageOpen, setIsPopupWithImageOpen] = React.useState(false)
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [selectedDeletionCard, setSelectedDeletionCard] = React.useState('')
+  const [cards, setCards] = React.useState([])
+
+  const [buttonSave, setButtonSave] = React.useState({load: false, title: 'Сохранить'});
+  const [buttonCreate, setButtonCreate] = React.useState({load: false, title: 'Создать'});
+  const [buttonDelete, setButtonDelete] = React.useState({load: false, title: 'Да'})
+
+  const [isButtonSaveUser, setIsButtonSaveUser] = React.useState(false);
+  const [isButtonSavePlace, setIsButtonSavePlace] = React.useState(false);
+  const [isButtonSaveAvatar, setIsButtonSaveAvatar] = React.useState(false);
+
+  const [isLoading, setIsloading] = React.useState(false);
+
+  const handleStateButtonEdit = (boolean) => {
+    setIsButtonSaveUser(boolean);
+  }
+
+  const handleStateButtonPlace = (boolean) => {
+    setIsButtonSavePlace(boolean);
+  }
+
+  const handleStateButtonAvatar = (boolean) => {
+    setIsButtonSaveAvatar(boolean);
+  }
+
+  React.useEffect(() =>{
+    api.getUser()
+      .then((res) => {
+        setCurrentUser(res);
+      },)
+      .catch(err => console.log(err))
+    setIsloading(true)
+    api.getInitialCards()
+      .then((res) => {
+        setCards(res);
+        setIsloading(false)
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  },[])
+
+  const handleButtonState = (setButton, boolean ,buttonTitle) => {
+    setButton({load: boolean, title: buttonTitle});
+  }
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    api.changeLikeCardStatus(card._id, isLiked)
+      .then((newCard) => {
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+      })
+      .catch(err => console.log(err));
+  }
+
+  function handleCardDelete(card) {
+    handleButtonState(setButtonDelete, true, 'Удаление...')
+    api.removeCard(card._id)
+      .then((res) => {
+        const newCards = cards.filter(newCard => newCard._id !== card._id);
+        setCards(newCards);
+        closeAllPopups();
+        handleButtonState(setButtonDelete, true, 'Да')
+      })
+      .catch(err => console.log(err))
+  }
+
+  const handleAddPlace = (name, link) => {
+    handleButtonState(setButtonCreate,true, 'Создание...')
+    api.addCard(name, link)
+      .then(res => {
+        setCards([res, ...cards]);
+        closeAllPopups();
+        handleButtonState(setButtonCreate,false, 'Создать')
+      })
+      .catch(err => console.log(err))
+  }
+
+  const handleEditAvatarClick = () => {
+    setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen);
+  }
+
+  const handleEditProfileClick = () => {
+    setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
+  }
+
+  const handleAddPlaceClick = () => {
+    setIsAddPlacePopupOpen(!isAddPlacePopupOpen);
+  }
+
+  const handleCardClick = (card) => {
+    setSelectedCard(card);
+    setIsPopupWithImageOpen(true)
+  }
+
+  const handleDeleteSelectedCard = (card) => {
+    setSelectedDeletionCard(card);
+  }
+
+  const handleBasketClick = () => {
+    setIsCardDeletePopupOpen(!isCardDeletePopupOpen);
+  }
+
+  const escClose = (evt) => {
+      if (evt.key === 'Escape') {
+        closeAllPopups();
+      }
+  }
+
+  const handleEscClose = (isOpen) => {
+    if(isOpen) document.addEventListener('keydown', escClose);
+  }
+
+  const handlePressingMouse = (evt) => {
+      if (evt.target.classList.contains('popup')) {
+        closeAllPopups();
+      }
+  }
+
+  //Параметры передаются для плавного закрытия попапа с изображением
+  const closeAllPopups = () => {
+    document.removeEventListener("keydown", escClose)
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsCardDeletePopupOpen(false);
+    setIsPopupWithImageOpen(false);
+  }
+
+  const handleUpdateUser = ({name, about}) => {
+    handleButtonState(setButtonSave, true,'Сохранение...')
+    api.editUserInfo(name, about)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+        handleButtonState(setButtonSave, false,'Сохранить')
+      })
+  }
+
+  const handleUpdateAvatar = (avatar) => {
+    handleButtonState(setButtonSave, true,'Сохранение...')
+    api.changeAvatar(avatar)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+        handleButtonState(setButtonSave, false,'Сохранить')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <div className="page">
+          <Header />
+          <Main
+            onEditProfile={handleEditProfileClick}
+            onAddPlace={handleAddPlaceClick}
+            onEditAvatar={handleEditAvatarClick}
+            onCardClick={handleCardClick}
+            onBasketClick={handleBasketClick}
+            cards={cards}
+            onCardLike={handleCardLike}
+            onCardDelete={handleDeleteSelectedCard}
+            isLoading={isLoading}
+          />
+          <Footer />
+        </div>
+        <EditProfilePopup
+          name='edit'
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onEscClose={handleEscClose}
+          onOverlayClose={handlePressingMouse}
+          onUpdateUser={handleUpdateUser}
+          onButtonActive={handleStateButtonEdit}
+          isButtonActive={isButtonSaveUser}
+          buttonTitle={buttonSave}
+
+        />
+        <AddPlacePopup
+          name='add'
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onEscClose={handleEscClose}
+          onOverlayClose={handlePressingMouse}
+          onAddPlace={handleAddPlace}
+          buttonTitle={buttonCreate}
+          onButtonActive={handleStateButtonPlace}
+          isButtonActive={isButtonSavePlace}
+        />
+        <EditAvatarPopup
+          name='avatar'
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onEscClose={handleEscClose}
+          onOverlayClose={handlePressingMouse}
+          buttonTitle={buttonSave}
+          onUpdateAvatar={handleUpdateAvatar}
+          onButtonActive={handleStateButtonAvatar}
+          isButtonActive={isButtonSaveAvatar}
+        />
+        <CardDeletePopup
+          isOpen={isCardDeletePopupOpen}
+          onClose={closeAllPopups}
+          onEscClose={handleEscClose}
+          onOverlayClose={handlePressingMouse}
+          onCardDelete={handleCardDelete}
+          card={selectedDeletionCard}
+          buttonTitle={buttonDelete}
+        />
+        <ImagePopup
+          card={selectedCard}
+          onClose={closeAllPopups}
+          onEscClose={handleEscClose}
+          isOpen={isPopupWithImageOpen}
+          onOverlayClose={handlePressingMouse}
+        />
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/EditProfilePopup.js
@@ -0,0 +1,82 @@
+import PopupWithForm from "./PopupWithForm";
+import React from "react";
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
+import Input from "./Input";
+function EditProfilePopup(props) {
+
+  const [name, setName]  = React.useState('вв');
+  const [description, setDescription] = React.useState( '');
+  const currentUser = React.useContext(CurrentUserContext);
+
+  function handleChangeName(e) {
+   setName(e.target.value)
+  }
+
+  function handleChangeDescription(e) {
+    setDescription(e.target.value)
+  }
+
+  React.useEffect(() => {
+    setName(currentUser.name);
+    setDescription(currentUser.about);
+  }, [currentUser, props.isOpen]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.onUpdateUser({
+      name: name.trim(),
+      about: description.trim(),
+    });
+  }
+
+  return(
+    <PopupWithForm
+      name='edit'
+      isOpen={props.isOpen}
+      onClose={props.onClose}
+      onEscClose={props.onEscClose}
+      onOverlayClose={props.onOverlayClose}
+      buttonTitle={props.buttonTitle}
+      onSubmit={handleSubmit}
+      isButtonActive={props.isButtonActive}
+      onButtonActive={props.onButtonActive}
+      children={(
+      <>
+        <h2 className="popup__title">Редактировать профиль</h2>
+        <label className="popup__form-field">
+          <Input
+            name='name'
+            type='text'
+            placeholder='Имя'
+            minLength='2'
+            maxLength='40'
+            xName={name}
+            value={name || ''}
+            onChange={handleChangeName}
+            isOpen={props.isOpen}
+            isButtonActive={props.isButtonActive}
+            onButtonActive={props.onButtonActive}
+            />
+        </label>
+        <label className="popup__form-field">
+          <Input
+            name='about'
+            type='text'
+            placeholder='Имя'
+            minLength='2'
+            maxLength='200'
+            xName={description}
+            value={description || ''}
+            onChange={handleChangeDescription}
+            isOpen={props.isOpen}
+            isButtonActive={props.isButtonActive}
+            onButtonActive={props.onButtonActive}
+          />
+        </label>
+      </>
+    )}
+    />
+  )
+}
+
+export default EditProfilePopup;
